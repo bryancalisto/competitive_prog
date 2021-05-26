186068e32826da9965ea29f8d4e0ced580ce83cd
Bryan Calisto <bryancalisto@yahoo.es>
2021-05-26 00:44:14 -0500

    Refactor: Improved jump_game_vii. Now it's faster

--- leetcode/jump_game_vii/main.cpp
+++ leetcode/jump_game_vii/main.cpp
@@ -5,28 +5,27 @@ using namespace std;
 
 bool canReach(string s, int minJump, int maxJump)
 {
-  int farthest = 0, start, i, j, k;
-  deque<int> steps = {0};
+  queue<int> steps;
+  int farthest = 0, start = 0, i, j;
+  steps.push(0);
 
   while (!steps.empty())
   {
-    i = steps.back();
-    steps.pop_back();
+    i = steps.front();
+    steps.pop();
     start = max(i + minJump, farthest + 1);
-    k = 0;
 
-    for (j = start; j < min(i + maxJump + 1, (int)s.size()); j++)
+    for (j = start; j < min(i + maxJump + 1, (int)s.length()); j++)
     {
       if (s[j] == '0')
       {
-        steps.push_front(j);
-        if (j == s.size() - 1)
+        steps.push(j);
+        if (j == s.length() - 1)
         {
           return true;
         }
       }
     }
-
     farthest = i + maxJump;
   }
 
