878ba07eeef9eacd78343fbc7a18734283cd8eac
Bryan Calisto <bryancalisto@yahoo.es>
2021-12-25 09:14:35 -0500

    New: Added solutions

---
+++ leetcode/bin_tree_postord_travers(145)/solution.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    res = []
+
+    def helper(self, root: Optional[TreeNode]):
+        if root == None:
+            return
+
+        self.helper(root.left)
+        self.helper(root.right)
+        self.res.append(root.val)
+
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        self.res = []
+        self.helper(root)
+        return self.res
+
+
+root = TreeNode(1, None, TreeNode(2, TreeNode(3)))
+print(Solution().postorderTraversal(root))
