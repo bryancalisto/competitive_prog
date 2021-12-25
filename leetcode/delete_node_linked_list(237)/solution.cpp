41f50d22c32d1a5a0b2fb3f735d17ba707281d51
Bryan Calisto <bryancalisto@yahoo.es>
2021-12-25 09:13:06 -0500

    New: Added solution

---
+++ leetcode/delete_node_linked_list(237)/solution.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+  void deleteNode(ListNode *node)
+  {
+    while (node->next->next)
+    {
+      node->val = node->next->val;
+      node = node->next;
+    }
+
+    node->val = node->next->val;
+    node->next = NULL;
+    delete node->next;
+  }
+};
+
+int main()
+{
+  Solution sol;
+  ListNode *root = new ListNode(4);
+  root->next = new ListNode(5);
+  root->next->next = new ListNode(1);
+  root->next->next->next = new ListNode(9);
+  root->next->next->next->next = nullptr;
+  ListNode *toDelete = root->next;
+
+  sol.deleteNode(toDelete);
+
+  while (root)
+  {
+    cout << root->val << endl;
+    root = root->next;
+  }
+}
