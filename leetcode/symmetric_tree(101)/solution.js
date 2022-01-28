4917b0f8468dfe5f9df25cec485e918580c30cdf
Bryan Calisto <bryancalisto@yahoo.es>
2022-01-27 19:25:04 -0500

    Refactor: Made a simpler solution

--- leetcode/symmetric_tree(101)/solution.js
+++ leetcode/symmetric_tree(101)/solution.js
@@ -7,22 +7,15 @@ function TreeNode(val, left, right) {
 var helper = function (left, right) {
   if (!left && !right) return true
 
-  if (left.val !== right.val || (!left && right) || (!right && left)) return false
+  if (!(left && right)) return false
 
-  const res1 = helper(left.left, right.right);
-  const res2 = helper(left.right, right.left);
-
-  if (res1 === false || res2 === false) {
-    return false
-  }
-
-  return
+  return left.val === right.val && helper(left.left, right.right) && helper(left.right, right.left);
 };
 
 var isSymmetric = function (root) {
   return helper(root.left, root.right)
 };
 
-const root = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3)));
-// const root = new TreeNode(1, new TreeNode(2, null, new TreeNode(3)), new TreeNode(2, null, new TreeNode(3)));
+// const root = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+const root = new TreeNode(1, new TreeNode(2, null, new TreeNode(3)), new TreeNode(2, null, new TreeNode(3)));
 console.log(isSymmetric(root));
