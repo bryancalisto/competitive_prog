fdbd9614cc8a400a398cbe026e2b3ce44c7ac447
Bryan Calisto <bryancalisto@yahoo.es>
2022-02-20 20:02:38 -0500

    Fix: Added null nodes support

--- leetcode/helpers/bTreeGenerator.js
+++ leetcode/helpers/bTreeGenerator.js
@@ -20,13 +20,19 @@ const generateBTree = (arr) => {
   while (valQueue.length > 0) {
     const currentNode = nodeQueue.shift();
 
-    left = new TreeNode(valQueue.shift());
-    currentNode.left = left;
-    nodeQueue.push(left);
+    let currentVal = valQueue.shift();
+    if (currentVal) {
+      left = new TreeNode(currentVal);
+      currentNode.left = left;
+      nodeQueue.push(left);
+    }
 
-    right = new TreeNode(valQueue.shift());
-    currentNode.right = right;
-    nodeQueue.push(right);
+    currentVal = valQueue.shift();
+    if (currentVal) {
+      right = new TreeNode(currentVal);
+      currentNode.right = right;
+      nodeQueue.push(right);
+    }
   }
 
   return node;
