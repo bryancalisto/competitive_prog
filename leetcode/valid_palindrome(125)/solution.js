784ac038e3ae794c5bf56c297c1beafecd356346
Bryan Calisto <bryancalisto@yahoo.es>
2022-03-03 20:20:18 -0500

    Refactor: Improved to time O(n/2)

--- leetcode/valid_palindrome(125)/solution.js
+++ leetcode/valid_palindrome(125)/solution.js
@@ -1,23 +1,30 @@
 var isPalindrome = function (s) {
   const stack = [];
   const queue = [];
-  let char;
+  let i = 0;
+  let j = s.length - 1;
 
-  for (let i = 0; i < s.length; i++) {
-    char = s[i].toLowerCase();
-    if (/[a-z0-9]/i.test(char)) {
-      queue.push(char);
-    }
-  }
+  while (i <= j) {
+    const char0 = s[i].toLowerCase();
 
-  for (let i = 0; i < s.length; i++) {
-    char = s[i].toLowerCase();
-    if (/[a-z0-9]/i.test(char)) {
-      let comparing = queue.pop();
-      if (comparing !== char) {
-        return false;
-      }
+    if (!/[a-z0-9]/i.test(char0)) {
+      i++;
+      continue;
     }
+
+    const char1 = s[j].toLowerCase();
+    if (!/[a-z0-9]/i.test(char1)) {
+      j--;
+      continue;
+    }
+
+    console.log(char0, char1);
+    if (char0 !== char1) {
+      return false;
+    }
+
+    i++;
+    j--;
   }
 
   return true;
