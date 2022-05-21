082c7b728743616f02fae86e3fc286a8d475bc18
Bryan Calisto <bryancalisto@yahoo.es>
2022-05-20 23:46:20 -0500

    New: DFS Solution

---
+++ leetcode/invert_bin_tree(226)/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+  TreeNode *invertTree(TreeNode *root)
+  {
+    if (!root)
+    {
+      return nullptr;
+    }
+
+    if (!root->left && !root->right)
+    {
+      return root;
+    }
+
+    TreeNode *left = invertTree(root->left);
+    TreeNode *right = invertTree(root->right);
+
+    root->left = right;
+    root->right = left;
+
+    return root;
+  }
+};
